283b2ad7b5d2756d9041b64084d505d3607802d6
und3r <73044953+omwfh@users.noreply.github.com>
2022-06-13 16:04:50 -0700

    Update SilentAim.lua

--- SilentAim.lua
+++ SilentAim.lua
@@ -42,3 +42,23 @@ r.Volume = 500
 r.SoundId = "rbxassetid://9314705681"
 r.Name= " "
 r:Play()
+
+local Client = game:GetService("Players").LocalPlayer
+local Character = Client.Character or Client.CharacterAdded:Wait()
+
+pcall(function()
+	while _G.Kimbladi == true do
+		local Animate = Character.Animate
+		Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=782841498"
+		Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=782841498"
+		Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+		Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+		Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=1083218792"
+		Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=1083439238"
+		Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=707829716"
+
+		game.Players.LocalPlayer.Character.Humanoid.Jump = false
+
+		wait(1)
+	end
+end)
