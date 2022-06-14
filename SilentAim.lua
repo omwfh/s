d680aa0540138c28a9f2a8ad5c2e95554ff1095b
und3r <73044953+omwfh@users.noreply.github.com>
2022-06-13 17:02:55 -0700

    Update SilentAim.lua

--- SilentAim.lua
+++ SilentAim.lua
@@ -44,23 +44,21 @@ if _G.Kimbladi == true then
 		local Humanoid = Character:FindFirstChild("Humanoid")
 		local Animate = Character.Animate
 
-		UIS.InputBegan:Connect(function(input, isTyping)
-			if not isTyping then
-				if input.UserInputType == Enum.KeyCode.KeypadFive then
-					pcall(function()
-						wait(0.2)
-						Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=782841498"
-						Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=782841498"
-						Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
-						Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
-						Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=1083218792"
-						Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=1083439238"
-						Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=707829716"
+		UIS.InputBegan:Connect(function(input)
+			if input.KeyCode == Enum.KeyCode.KeypadFive then
+				pcall(function()
+					wait(0.2)
+					Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=782841498"
+					Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=782841498"
+					Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+					Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+					Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=1083218792"
+					Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=1083439238"
+					Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=707829716"
 
-						Humanoid.Jump = false
+					Humanoid.Jump = false
 
-					end)
-				end
+				end)
 			end
 		end)
 	end)
