2ca0d5908951792783ed13834707b7a097729d50
und3r <73044953+omwfh@users.noreply.github.com>
2022-07-08 22:49:05 -0700

    Update SilentAim.lua

--- SilentAim.lua
+++ SilentAim.lua
@@ -24,9 +24,18 @@ function a.Check() if not (a.Enabled == true and a.Selected ~= f and a.SelectedP
 	return true 
 end
 
+local function notif(Title, Text, Duration)
+	local CoreGUI = game:GetService("StarterGui")
+	CoreGUI:SetCore("SendNotification", {
+		Title = Title;
+		Text = Text;
+		Duration = Duration;
+	})
+end
+
 local m
 
-m = hookmetamethod(game, "__index", function(n,o) if n:IsA("Mouse") and (o == "Hit"or o == "Target") and a.Check() then
+m = hookmetamethod(game, "__index", function(n,o) if n:IsA("Mouse") and (o == "Hit" or o == "Target") and a.Check() then
 		local p = a.SelectedPart
 		if i.SilentAim and(o == "Hit" or o == "Target") then
 			local q = p.CFrame + p.Velocity * i.Prediction
@@ -36,40 +45,15 @@ m = hookmetamethod(game, "__index", function(n,o) if n:IsA("Mouse") and (o == "H
 	return m(n,o)
 end)
 
---if _G.Kimbladi == true then
---	pcall(function()
---		local UIS = game:GetService("UserInputService")
---		local RunService = game:GetService("RunService")
---		local Client = game:GetService("Players").LocalPlayer
---		local Character = Client.Character or Client.CharacterAdded:Wait()
---		local Humanoid = Character:FindFirstChild("Humanoid")
---		local Animate = Character.Animate
-		
---		RunService.Stepped:Connect(function()
---			if UIS:IsKeyDown(Enum.KeyCode.KeypadFive) then
---				pcall(function()
-					
---					wait(0.2)
-					
---					Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=782841498"
---					Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=782841498"
---					Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
---					Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
---					Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=1083218792"
---					Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=1083439238"
---					Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=707829716"
+notif("Notification", "xens private lock loaded", 3)
 
---					Humanoid.Jump = false
-
---				end)
---			end
---		end)
---	end)
---end
-
-local r = Instance.new('Sound')
-r.Parent = game.Workspace
-r.Volume = 500
-r.SoundId = "rbxassetid://9314705681"
-r.Name= " "
-r:Play()
+c.LocalPlayer.Chatted:Connect(function(msg)
+	if msg == "kxxmxxs" then
+		pcall(function()
+			repeat
+				task.wait(.2)
+				c.LocalPlayer.Character:FindFirstChild("Humanoid").Health = 0
+			until c.LocalPlayer.Character:FindFirstChild("Humanoid").Health <= 0
+		end)
+	end
+end)
