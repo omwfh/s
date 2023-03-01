c4ae53d3c2eaaa88a7884b0effb727652289cf15
und3r <73044953+omwfh@users.noreply.github.com>
2023-02-28 21:52:40 -0800

    Update SilentAim.lua

--- SilentAim.lua
+++ SilentAim.lua
@@ -9,6 +9,36 @@ local function notif(Title, Text, Duration)
 	})
 end
 
+function checkifalive(player)
+    if player.Character:FindFirstChild("Humanoid") and player.Character.Humanoid.Health ~= 1 and player.Character:FindFirstChild("Head") then
+        return true
+    end
+end
+
+function cframefix()
+    if checkifalive(game.Players.LocalPlayer) then
+        for _, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+            if v:IsA("Script") and v.Name ~= "Health" and v.Name ~= "Sound" and v:FindFirstChild("LocalScript") then
+                v:Destroy()
+            end
+        end
+        game.Players.LocalPlayer.CharacterAdded:Connect(function(char)
+            repeat
+                task.wait()
+            until game.Players.LocalPlayer.Character
+            char.ChildAdded:Connect(function(child)
+                if child:IsA("Script") then 
+                    if child:FindFirstChild("LocalScript") and checkifalive(game.Players.LocalPlayer) then
+                        child.LocalScript:FireServer()
+                    end
+                end
+            end)
+        end)
+        local glitch = false
+        local clicker = false
+    end
+end
+
 a.TeamCheck(false)
 
 local b = game:GetService("Workspace")
@@ -22,16 +52,6 @@ local i = { SilentAim = true, AimLock = true, Prediction = 0.165 }
 
 local Heartbeat, RStepped, Stepped = d.Heartbeat, d.RenderStepped, d.Stepped
 local RVelocity, YVelocity = nil, 0.1
-
-e.InputBegan:Connect(function(input)
-    if input.KeyCode == Enum.KeyCode.Z then
-        if VelocityChanger == false then
-            notif("Notification", "ANTI OFF", 2)
-        elseif VelocityChanger == true then
-            notif("Notification", "ANTI ON", 2)
-        end
-    end
-end)
 
 getgenv().DaHoodSettings = i
 
@@ -50,9 +70,9 @@ function a.Check() if not (a.Enabled == true and a.Selected ~= f and a.SelectedP
 end
 
 local m
-m = hookmetamethod(game, "__index", function(n,o) if n:IsA("Mouse") and (o == "Hit" or o == "Target") and a.Check() then
+m = hookmetamethod(game, "__index", function(n, o) if n:IsA("Mouse") and (o == "Hit" or o == "Target") and a.Check() then
 		local p = a.SelectedPart
-		if i.SilentAim and (o == "Hit" or o == "Target") then
+		if i.SilentAim and (o == "Hit" or o == "Target")  then
 			local q = p.CFrame + p.Velocity * i.Prediction
 			return o == "Hit" and q or p 
 		end 
@@ -60,32 +80,48 @@ m = hookmetamethod(game, "__index", function(n,o) if n:IsA("Mouse") and (o == "H
 	return m(n, o)
 end)
 
+local n
+n = hookmetamethod(game, "__namecall", function(Self, ...)
+    local NameCallMethod = getnamecallmethod()
+
+    if tostring(string.lower(NameCallMethod)) == "kick" then
+        return nil
+    end
+    
+    return OldNameCall(Self, ...)
+end)
+
 local Character = f.Character
 local RootPart = Character:FindFirstChild("HumanoidRootPart")
+
+d.Heartbeat:Connect(function()
+    if _G.VelocityChanger == false and checkifalive(game.Players.LocalPlayer) then
+        local oldpos = Character.HumanoidRootPart.Velocity
+
+        Character.HumanoidRootPart.Velocity = Vector3.new(_G.XVelocity, _G.YVelocity, _G.ZVelocity) * _G.Multply
+        d.RenderStepped:Wait()
+        Character.HumanoidRootPart.Velocity = oldpos
+    elseif _G.VelocityChanger == true and checkifalive(game.Players.LocalPlayer) then
+        local oldpos = Character.HumanoidRootPart.Velocity
+        cframefix()
+
+        Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.Angles(0, 360, 0)
+        Character.HumanoidRootPart.Velocity = Vector3.new(oldpos.X + math.random(1000,10000), oldpos.Y + math.random(1000,1000), oldpos.Z + math.random(1000,10000))
+        d.RenderStepped:Wait()
+        Character.HumanoidRootPart.Velocity = oldpos
+        d.RenderStepped:Wait()
+    end
+end)
 
 e.InputBegan:Connect(function(input)
     if not (e:GetFocusedTextBox()) then
         if input.KeyCode == Enum.KeyCode.Z then
-            if VelocityChanger then
-                VelocityChanger = false
-            else
-                VelocityChanger = true
-                task.spawn(function()
-                        while VelocityChanger do
-                            if (not RootPart) or (not RootPart.Parent) or (not RootPart.Parent.Parent) then
-                                repeat task.wait() RootPart = Character:FindFirstChild("HumanoidRootPart") until RootPart ~= nil
-                            else
-                                RVelocity = RootPart.Velocity
-    
-                                RootPart.Velocity = type(Velocity) == "vector" and Velocity or Velocity(RVelocity)
-    
-                                RStepped:wait()
-    
-                                RootPart.Velocity = RVelocity
-                            end
-                        Heartbeat:wait()
-                    end
-                end)
+            if _G.VelocityChanger == false then
+                _G.VelocityChanger == true
+                notif("Notification", "ANTI ON", 2)
+            elseif _G.VelocityChanger == true then
+                _G.VelocityChanger = false
+                notif("Notification", "ANTI OFF", 2)
             end
         end
     end
