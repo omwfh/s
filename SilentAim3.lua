4a39b6ba9aeb0c8a87f7229b48d3447375e141ce
und3r <73044953+omwfh@users.noreply.github.com>
2024-07-28 20:08:30 -0700

    Update SilentAim3.lua

--- SilentAim3.lua
+++ SilentAim3.lua
@@ -41,94 +41,6 @@ function a.Check()
     return true 
 end
 
--- Define utility functions
-function getrawmetatable(obj)
-    local metatable = getmetatable(obj)
-    if not metatable then return nil end
-
-    local metatables = {}
-    while metatable do
-        table.insert(metatables, metatable)
-        metatable = getmetatable(metatable)
-    end
-    return metatables
-end
-
-function setrawmetatable(obj, mt)
-    if type(obj) == 'table' and type(mt) == 'table' then
-        setmetatable(obj, mt)
-        return true
-    else
-        return false, "both arguments must be tables."
-    end
-end
-
-function setreadonly(obj, val)
-    if type(obj) == 'table' then
-        local mt = getmetatable(obj) or {}
-        if val then
-            mt.__newindex = function(t, k, v)
-                error("attempt to modify readonly table", 2)
-            end
-        else
-            mt.__newindex = nil
-        end
-        setmetatable(obj, mt)
-    else
-        error("expected a table for the first argument", 2)
-    end
-end
-
-function isreadonly(obj)
-    if type(obj) == 'table' then
-        local mt = getmetatable(obj)
-        if mt and mt.__newindex then
-            return false
-        end
-        return true
-    else
-        error("expected a table for the argument", 2)
-    end
-end
-
-function make_readonly(obj)
-    if type(obj) == 'table' then
-        local mt = getmetatable(obj) or {}
-        mt.__newindex = function(t, k, v)
-            error("attempt to modify readonly table", 2)
-        end
-        setmetatable(obj, mt)
-    else
-        error("expected a table for the first argument", 2)
-    end
-end
-
-function make_writeable(obj)
-    if type(obj) == 'table' then
-        local mt = getmetatable(obj) or {}
-        mt.__newindex = nil
-        setmetatable(obj, mt)
-    else
-        error("expected a table for the first argument", 2)
-    end
-end
-
--- Hookmetamethod function
-function hookmetamethod(object, metamethod, hook)
-    local mt = getmetatable(object)
-    if not mt then
-        error("Object does not have a metatable")
-    end
-
-    local original = mt[metamethod]
-
-    mt[metamethod] = function(...)
-        return hook(original, ...)
-    end
-
-    return original
-end
-
 -- Define the hook function
 local function hookFunction(original, n, o)
     if n:IsA("Mouse") and (o == "Hit" or o == "Target") and a.Check() then
@@ -141,12 +53,19 @@ local function hookFunction(original, n, o)
     return original(n, o)
 end
 
--- Hook the __index metamethod
-local mt = getrawmetatable(game)
-if mt then
-    make_writeable(mt[1])
-    hookmetamethod(game, "__index", hookFunction)
-    make_readonly(mt[1])
+-- Create a proxy for the game object
+local gameMeta = getrawmetatable(game)
+local oldIndex = gameMeta.__index
+
+gameMeta.__index = function(self, key)
+    if self:IsA("Mouse") and (key == "Hit" or key == "Target") and a.Check() then
+        local p = a.SelectedPart
+        if i.SilentAim and (key == "Hit" or key == "Target") then
+            local q = p.CFrame + p.Velocity * i.Prediction
+            return key == "Hit" and q or p
+        end
+    end
+    return oldIndex(self, key)
 end
 
 -- Notification
