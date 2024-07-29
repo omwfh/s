e3a58fe2164d67cdd4d36a7251b2f30cea0ce430
und3r <73044953+omwfh@users.noreply.github.com>
2024-07-28 19:56:04 -0700

    Create SilentAim3.lua

---
+++ SilentAim3.lua
@@ -0,0 +1,72 @@
+local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/omwfh/sj0wjg0w/main/AimingModule.lua"))()
+
+local function notif(Title, Text, Duration)
+	local CoreGUI = game:GetService("StarterGui")
+	CoreGUI:SetCore("SendNotification", {
+		Title = Title;
+		Text = Text;
+		Duration = Duration;
+	})
+end
+
+a.TeamCheck(false)
+
+local b = game:GetService("Workspace")
+local c = game:GetService("Players")
+local d = game:GetService("RunService")
+local e = game:GetService("UserInputService")
+local f = c.LocalPlayer
+local g = f:GetMouse()
+local h = b.CurrentCamera
+local i = { SilentAim = true, AimLock = true, Prediction = 0.165 }
+
+getgenv().DaHoodSettings = i
+
+function a.Check() if not (a.Enabled == true and a.Selected ~= f and a.SelectedPart ~= nil) then
+	return false 
+end
+
+local j = a.Character(a.Selected)
+local k = j:WaitForChild("BodyEffects")["K.O"].Value
+local l = j:FindFirstChild("GRABBING_CONSTRAINT") ~= nil
+
+if k or l then return false end
+	return true 
+end
+
+function hookmetamethod(object, metamethod, hook)
+    -- Get the metatable of the object
+    local mt = getmetatable(object)
+    if not mt then
+        error("Object does not have a metatable")
+    end
+
+    -- Save the original metamethod
+    local original = mt[metamethod]
+
+    -- Set the new metamethod
+    mt[metamethod] = function(...)
+        return hook(original, ...)
+    end
+
+    -- Return the original metamethod in case you need to unhook later
+    return original
+end
+
+-- Define the hook function
+local function hookFunction(original, n, o)
+    if n:IsA("Mouse") and (o == "Hit" or o == "Target") and a.Check() then
+        local p = a.SelectedPart
+        if i.SilentAim and (o == "Hit" or o == "Target") then
+            local q = p.CFrame + p.Velocity * i.Prediction
+            return o == "Hit" and q or p
+        end
+    end
+    return original(n, o)
+end
+
+-- Hook the __index metamethod
+local m
+m = hookmetamethod(game, "__index", hookFunction)
+
+notif("Notification", "arts private lock loaded", 5)
